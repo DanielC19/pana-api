d84a626a68a51818111b48c7361e1555f19fb89a
DanielC19 <danysam1010@gmail.com>
2021-11-04 21:43:23 -0500

    Routing
    All API routes were created in groups by its prefix

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\PearlsController;
+use App\Http\Controllers\PlaylistController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SayingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('pearl')->group(function () {
+
+    Route::post('/daily',    [PearlsController::class], 'daily');
+    Route::post('/all',      [PearlsController::class], 'all');
+    Route::post('/create',   [PearlsController::class], 'create');
+
+});
+
+Route::prefix('saying')->group(function () {
+
+    Route::post('/random',   [SayingController::class], 'random');
+    Route::post('/all',      [SayingController::class], 'all');
+    Route::post('/create',   [SayingController::class], 'create');
+
+});
+
+Route::prefix('playlist')->group(function () {
+
+    Route::post('/me',          [PlaylistController::class], 'me');
+    Route::get('/user/{id}',    [PlaylistController::class], 'user');
+    Route::post('/all',         [PlaylistController::class], 'all');
+    Route::post('/create',      [PlaylistController::class], 'create');
+    Route::get('/delete/{id}',  [PlaylistController::class], 'delete');
+
+});
+
+Route::prefix('profile')->group(function () {
+    
+    Route::post('create',               [ProfileController::class], 'create');
+    Route::post('me',                   [ProfileController::class], 'me');
+    Route::get('user/{id}',             [ProfileController::class], 'user');
+    Route::post('add/nickname',         [ProfileController::class], 'nickname');
+    Route::post('add/role',             [ProfileController::class], 'role');
+    Route::post('add/wanted',           [ProfileController::class], 'wanted');
+    Route::post('add/birth',            [ProfileController::class], 'birth');
+    Route::post('add/games',            [ProfileController::class], 'games');
+    Route::post('add/icfes',            [ProfileController::class], 'icfes');
+    Route::post('add/memorable',        [ProfileController::class], 'memorable');
+    Route::post('add/reprehensible',    [ProfileController::class], 'reprehensible');
+    Route::post('add/entry',            [ProfileController::class], 'entry');
+
 });
