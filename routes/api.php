540ff90c0e5ba269e9c7c7b6a643b5a5e7aa84c9
DanielC19 <danysam1010@gmail.com>
2021-11-04 22:09:05 -0500

    Fix Routes
    All routes were fixed

--- routes/api.php
+++ routes/api.php
@@ -7,56 +7,45 @@ use App\Http\Controllers\SayingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::prefix('pearl')->group(function () {
 
-    Route::post('/daily',    [PearlsController::class], 'daily');
-    Route::post('/all',      [PearlsController::class], 'all');
-    Route::post('/create',   [PearlsController::class], 'create');
+    Route::post('/daily',    [PearlsController::class, 'daily']);
+    Route::post('/all',      [PearlsController::class, 'all']);
+    Route::post('/create',   [PearlsController::class, 'create']);
 
 });
 
 Route::prefix('saying')->group(function () {
 
-    Route::post('/random',   [SayingController::class], 'random');
-    Route::post('/all',      [SayingController::class], 'all');
-    Route::post('/create',   [SayingController::class], 'create');
+    Route::post('/random',   [SayingController::class, 'random']);
+    Route::post('/all',      [SayingController::class, 'all']);
+    Route::post('/create',   [SayingController::class, 'create']);
 
 });
 
 Route::prefix('playlist')->group(function () {
 
-    Route::post('/me',          [PlaylistController::class], 'me');
-    Route::get('/user/{id}',    [PlaylistController::class], 'user');
-    Route::post('/all',         [PlaylistController::class], 'all');
-    Route::post('/create',      [PlaylistController::class], 'create');
-    Route::get('/delete/{id}',  [PlaylistController::class], 'delete');
+    Route::post('/me',          [PlaylistController::class, 'me']);
+    Route::get('/user/{id}',    [PlaylistController::class, 'user']);
+    Route::post('/all',         [PlaylistController::class, 'all']);
+    Route::post('/create',      [PlaylistController::class, 'create']);
+    Route::get('/delete/{id}',  [PlaylistController::class, 'delete']);
 
 });
 
 Route::prefix('profile')->group(function () {
     
-    Route::post('create',               [ProfileController::class], 'create');
-    Route::post('me',                   [ProfileController::class], 'me');
-    Route::get('user/{id}',             [ProfileController::class], 'user');
-    Route::post('add/nickname',         [ProfileController::class], 'nickname');
-    Route::post('add/role',             [ProfileController::class], 'role');
-    Route::post('add/wanted',           [ProfileController::class], 'wanted');
-    Route::post('add/birth',            [ProfileController::class], 'birth');
-    Route::post('add/games',            [ProfileController::class], 'games');
-    Route::post('add/icfes',            [ProfileController::class], 'icfes');
-    Route::post('add/memorable',        [ProfileController::class], 'memorable');
-    Route::post('add/reprehensible',    [ProfileController::class], 'reprehensible');
-    Route::post('add/entry',            [ProfileController::class], 'entry');
+    Route::post('create',               [ProfileController::class, 'create']);
+    Route::post('me',                   [ProfileController::class, 'me']);
+    Route::get('user/{id}',             [ProfileController::class, 'user']);
+    Route::post('add/nickname',         [ProfileController::class, 'nickname']);
+    Route::post('add/role',             [ProfileController::class, 'role']);
+    Route::post('add/wanted',           [ProfileController::class, 'wanted']);
+    Route::post('add/birth',            [ProfileController::class, 'birth']);
+    Route::post('add/games',            [ProfileController::class, 'games']);
+    Route::post('add/icfes',            [ProfileController::class, 'icfes']);
+    Route::post('add/memorable',        [ProfileController::class, 'memorable']);
+    Route::post('add/reprehensible',    [ProfileController::class, 'reprehensible']);
+    Route::post('add/entry',            [ProfileController::class, 'entry']);
 
 });
