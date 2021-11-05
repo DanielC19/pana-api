632c07444a775f54229918238fd139f915776ea1
DanielC19 <danysam1010@gmail.com>
2021-11-04 21:18:36 -0500

    Migration - FK corrections
    Some Foreign Keys were updated, so all of them are related to the "user_id" and not to the profile anymore

--- database/migrations/2021_11_01_184024_create_nicknames_table.php
+++ database/migrations/2021_11_01_184024_create_nicknames_table.php
@@ -15,8 +15,8 @@ class CreateNicknamesTable extends Migration
     {
         Schema::create('nicknames', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('profile_id')->unsigned();
-            $table->foreign('profile_id')->references('id')->on('profiles');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('nickname');
             $table->timestamps();
         });
