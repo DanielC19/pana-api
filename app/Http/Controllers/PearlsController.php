b92d8b93b198435513a9a16a7a83c3a1407c0a73
DanielC19 <danysam1010@gmail.com>
2021-11-04 21:24:54 -0500

    Base Models and Controllers
    Raw models and controllers needed for the API

---
+++ app/Http/Controllers/PearlsController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PearlsController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/SayingController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SayingController extends Controller
+{
+    //
+}
