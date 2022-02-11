e53cb677d1bb91ed3a1db57f73ba9ba877f9e8c2
DanielC19 <danysam1010@gmail.com>
2022-02-10 20:33:35 -0500

    Feature: Daily log - Model and migration
    - Add model and migration to table "daily"

---
+++ app/Models/Daily.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Daily extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'pearl_id'
+    ];
+
+}
