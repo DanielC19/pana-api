8f494285ff203b97d7d2c693125cbe16de15b82c
DanielC19 <danysam1010@gmail.com>
2022-02-10 20:33:59 -0500

    Feature: Pearls model

--- app/Models/Pearls.php
+++ app/Models/Pearls.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Pearls extends Model
 {
     use HasFactory;
+
+    static $rules = [
+        'number' => 'integer|required',
+        'phrase' => 'string|required|max:255|unique:pearls,phrase',
+        'author' => 'string|required|max:255',
+        'date' => 'string|required|max:255'
+    ];
+
+    protected $fillable = [
+        'number',
+        'phrase',
+        'author',
+        'date'
+    ];
 }
