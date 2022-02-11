c8f582c028b885ec6b4523319784cccf7af1c64c
DanielC19 <danysam1010@gmail.com>
2022-02-10 23:09:18 -0500

    Feature: Playlist functionality
    - Add model and relationship of playlist.
    - Add all functions of playlist to the controller.

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Playlist extends Model
 {
     use HasFactory;
+
+    static $rules = [
+        'name' => 'string|required|max:255',
+        'link' => 'string|required|max:255'
+    ];
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'link'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
